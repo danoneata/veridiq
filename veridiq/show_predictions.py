523c2416b4c9220aed9e5213af35a76579315757
Dan Oneata <dan.oneata@gmail.com>
2025-07-15 10:50:11 +0300

    Output maximization

--- veridiq/show_predictions.py
+++ veridiq/show_predictions.py
@@ -35,7 +35,7 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import BinaryClassifierOutputTarget
 
 from veridiq.data import AV1M
-from veridiq.utils import cache_np, implies
+from veridiq.utils import cache_json, cache_np, implies
 import veridiq.fsfm.models_vit
 import veridiq.mymarkdown as md
 
@@ -597,7 +597,9 @@ def get_grad_cam_model(feature_extractor_type="CLIP", **kwargs):
 
 
 def get_predictions_path(feature_extractor_type):
-    return "output/clip-linear/predictions-{}.npy".format(feature_extractor_type.lower())
+    return "output/clip-linear/predictions-{}.npy".format(
+        feature_extractor_type.lower()
+    )
 
 
 def show_temporal_explanations():
@@ -725,6 +727,108 @@ def show_spatial_explanations():
                 st.markdown("---")
 
 
+def show_frames_classifier_maximization():
+    st.set_page_config(layout="wide")
+
+    with st.sidebar:
+        feature_extractor_type = st.selectbox(
+            "Feature Extractor",
+            options=["CLIP", "FSFM"],
+            index=0,
+        )
+        n_frames = st.number_input(
+            "Number of frames to show",
+            min_value=5,
+            max_value=150,
+            value=30,
+            step=5,
+        )
+        maximize = st.selectbox(
+            "Frame selection",
+            options=["Highest scores", "Lowest scores"],
+            index=0,
+        )
+
+    model_classifier = load_model_classifier(feature_extractor_type)
+
+    metadata0 = load_test_metadata(feature_extractor_type)
+    features0 = load_test_features(feature_extractor_type)
+    path = get_predictions_path(feature_extractor_type)
+    preds = cache_np(path, compute_predictions, model_classifier, features0)
+
+    preds, metadata = select_rvra_or_fvfa(preds, metadata0)
+    num_videos = len(preds)
+    to_maximize = maximize == "Highest scores"
+
+    def get_frame_data_1(i):
+        pred = preds[i]
+        frame_idxs = np.argsort(pred)
+        if to_maximize:
+            frame_idxs = frame_idxs[::-1]
+        frame_idxs = frame_idxs[:n_frames]
+        frame_idxs = frame_idxs.tolist()
+        return [
+            {
+                "video_idx": i,
+                "frame_idx": frame_idx,
+                "pred": pred[frame_idx].item(),
+            }
+            for frame_idx in frame_idxs
+        ]
+
+    def get_frame_data_all():
+        return [e for i in tqdm(range(num_videos)) for e in get_frame_data_1(i)]
+
+    def get_path():
+        return "output/frames-classifier-maximization-{}-{}.json".format(
+            feature_extractor_type.lower(),
+            "highest" if maximize == "Highest scores" else "lowest",
+        )
+
+    all_frame_data = cache_json(get_path(), get_frame_data_all)
+    all_frame_data = sorted(
+        all_frame_data,
+        key=lambda x: x["pred"],
+        reverse=to_maximize,
+    )
+    selected_frames = all_frame_data[:n_frames]
+
+    def get_info(frame_data):
+        video_idx = frame_data["video_idx"]
+        frame_idx = frame_data["frame_idx"]
+
+        datum = metadata[video_idx]
+        labels = get_per_frame_labels(datum)
+        label = labels[frame_idx]
+        label_str = "fake" if label == 1 else "real" if label == 0 else "unknown"
+
+        return md.ul(
+            [
+                # "video: `{}`".format(datum["file"]),
+                "frame: {:d} · time: {:.1f}s".format(
+                    frame_data["frame_idx"], index_to_time(frame_data["frame_idx"])
+                ),
+                "label: {}".format(label_str),
+                "pred: {:.3f}".format(frame_data["pred"]),
+            ]
+        )
+
+    def get_frame(datum):
+        video_idx = datum["video_idx"]
+        frame_idx = datum["frame_idx"]
+        frames = list(load_video_frames(metadata[video_idx]))
+        return frames[frame_idx]
+
+    # Display frames in a grid
+    n_cols = 5
+    for group in partition_all(n_cols, selected_frames):
+        cols = st.columns(len(group))
+        for j, frame_data in enumerate(group):
+            cols[j].markdown(get_info(frame_data))
+            cols[j].image(get_frame(frame_data))
+
+
 if __name__ == "__main__":
-    show_temporal_explanations()
-    # show_spatial_explanations()
+    # show_temporal_explanations()
+    show_spatial_explanations()
+    # show_frames_classifier_maximization()
