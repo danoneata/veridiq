bcaccba78ad8f08313cc3db396754437992089b1
Dan Oneata <dan.oneata@gmail.com>
2025-07-22 14:16:52 +0300

    WIP Add new dataset: ExDDV

--- veridiq/data.py
+++ veridiq/data.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+
 import json
+import pdb
+
+import pandas as pd
 
 
 class AV1M:
@@ -18,16 +22,41 @@ class AV1M:
         return str(path)
 
 
+
+class ExDDV:
+    def __init__(self, split):
+        pass
+
+    def load_metadata(self):
+        path = Path("/root/work/exddv/ExDDV.csv")
+        return pd.read_csv(path)
+
+
+
 DATASETS = {
     "av1m": AV1M,
+    "exddv": ExDDV,
 }
 
 
 if __name__ == "__main__":
-    import streamlit as st
+    # import streamlit as st
 
-    dataset = AV1M("val")
-    data = dataset.load_filelist()
+    # dataset = AV1M("val")
+    # data = dataset.load_filelist()
 
-    st.write(data[0])
-    st.video(dataset.get_video_path(data[0]["file"]))
+    # st.write(data[0])
+    # st.video(dataset.get_video_path(data[0]["file"]))
+
+    def func(x):
+        num_annotators = len(x.username.unique())
+        if num_annotators >= 2:
+            pdb.set_trace()
+        else:
+            return 1
+
+    dataset = ExDDV("val")
+    metadata = dataset.load_metadata()
+    print(metadata.columns)
+    pdb.set_trace()
+    metadata.groupby("movie_name").apply(func)
