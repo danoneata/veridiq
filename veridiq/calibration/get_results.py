14c17f2455fdb206167c2654695c1b2a81b1c850
Dan Oneata <dan.oneata@gmail.com>
2025-10-17 19:27:24 +0300

    Evaluate in terms of ECE

--- veridiq/calibration/get_results.py
+++ veridiq/calibration/get_results.py
@@ -7,8 +7,10 @@ import pandas as pd
 import numpy as np
 
 from sklearn.metrics import roc_auc_score
+from sklearn.calibration import calibration_curve
 
 from veridiq.linear_probing.train_test import evaluate_frame_level
+from veridiq.explanations.show_temporal_explanations import pred_to_proba
 
 
 DATASETS = [
@@ -19,6 +21,14 @@ DATASETS = [
 ]
 
 BASE_DIR = Path("output/training-linear-probing")
+
+
+def compute_ece(probs, labels, n_bins=10):
+    prob_true, prob_pred = calibration_curve(labels, probs, n_bins=n_bins)
+    B, _ = np.histogram(probs, bins=n_bins)
+    N = len(probs)
+    ece = np.sum(np.abs(prob_true - prob_pred) * B / N)
+    return ece
 
 
 def load_results_path(path):
@@ -38,19 +48,34 @@ def ensemble_results(dfs):
     return df
 
 
-def evaluate(df):
+def evaluate_auc(df):
     scores = df["scores"].to_numpy()
     labels = df["labels"].to_numpy()
     return 100 * roc_auc_score(y_score=scores, y_true=labels)
 
 
-def get_results_default(config_names, dataset):
+def evaluate_ece(df):
+    scores = df["scores"].to_numpy()
+    labels = df["labels"].to_numpy()
+    probas = pred_to_proba(scores)
+    ece = compute_ece(probas, labels, n_bins=10)
+    return 100 * ece
+
+
+EVAL_FUNCS = {
+    "auc": evaluate_auc,
+    "auc-frame-level": evaluate_frame_level,
+    "ece": evaluate_ece,
+}
+
+
+def get_results_default(config_names, dataset, evaluate):
     dfs = [load_results_single(config_name, dataset) for config_name in config_names]
     df = ensemble_results(dfs)
     return evaluate(df)
 
 
-def get_results_dfeval(config_names, dataset):
+def get_results_dfeval(config_names, dataset, evaluate):
     def get_path(config_name):
         return BASE_DIR / config_name / "predict" / "bitdf" / "results.csv"
 
@@ -63,7 +88,7 @@ def get_results_dfeval(config_names, dataset):
     return evaluate(df)
 
 
-def get_results_localization(config_names, dataset):
+def get_results_localization(config_names, dataset, evaluate):
     def load_frame_level_results(config_name):
         path = BASE_DIR / config_name / "predict" / dataset / "results-frame-level.json"
         with open(path, "r") as f:
@@ -85,7 +110,7 @@ def get_results_localization(config_names, dataset):
 
     data_all = [load_frame_level_results(c) for c in config_names]
     ensembled_data = [ensemble_datum(data) for data in zip(*data_all)]
-    return 100 * evaluate_frame_level(ensembled_data)
+    return 100 * evaluate(ensembled_data)
 
 
 GET_RESULTS_FUNCS = {
@@ -99,13 +124,15 @@ GET_RESULTS_FUNCS = {
 
 def get_results(config_names):
     RESULTS_CONFIGS = [
-        ("av1m", "av1m"),
-        ("av1m-loc", "av1m"),
-        ("favc", "favc"),
-        ("avlips", "avlips"),
-        ("dfeval", "dfeval"),
+        ("av1m", "av1m", evaluate_auc),
+        ("av1m-loc", "av1m", evaluate_frame_level),
+        ("favc", "favc", evaluate_auc),
+        ("avlips", "avlips", evaluate_auc),
+        ("dfeval", "dfeval", evaluate_auc),
     ]
-    return [GET_RESULTS_FUNCS[r](config_names, d) for r, d in RESULTS_CONFIGS]
+    results1 = [GET_RESULTS_FUNCS[r](config_names, d, e) for r, d, e in RESULTS_CONFIGS]
+    results2 = [GET_RESULTS_FUNCS[d](config_names, d, evaluate_ece) for d in DATASETS]
+    return results1 + results2
 
 
 @click.command()
